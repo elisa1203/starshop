111ee3dbc7820559735a37c786d3890efe5a932e
elisa1203 <elisajoan1203@gmail.com>
2025-12-12 16:03:03 +0900

    Add files via upload

---
+++ mobile.js
@@ -0,0 +1,7 @@
+<link id="style-sheet" rel="stylesheet" href="style.css">
+
+<script>
+  document.getElementById("switch-to-mobile").addEventListener("click", function() {
+    document.getElementById("style-sheet").href = "mobile.css";
+  });
+</script>
